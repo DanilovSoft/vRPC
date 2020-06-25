564a425a3e97326e7919ccc532817a837abe6383
Vitaliy Danilov <vitaliy931@gmail.com>
2020-06-26 00:00:59 +0300

    Without strong-name for CLI

--- src/Tests/IntegrationTests/ConsoleApp/Program.cs
+++ src/Tests/IntegrationTests/ConsoleApp/Program.cs
@@ -22,36 +22,6 @@ namespace ConsoleApp
     {
         static async Task Main()
         {
-            //HeaderDto h = HeaderDto.CreateRequest(int.MaxValue, int.MaxValue, "json", "Home/Test");
-            //while (true)
-            //{
-                //var arrayWriter = new ArrayBufferWriter<byte>(1024);
-            //    var sw = Stopwatch.StartNew();
-            //    h.SerializeJson(arrayWriter);
-            //    sw.Stop();
-            //    Trace.WriteLine("System.Text Ser: " + sw.ElapsedTicks);
-
-            //    string controlJ = Encoding.UTF8.GetString(arrayWriter.WrittenSpan);
-
-            //    var mem2 = new MemoryStream(64);
-            //    sw.Restart();
-            //    ProtoBuf.Serializer.Serialize<HeaderDto>(mem2, h);
-            //    sw.Stop();
-            //    Trace.WriteLine("ProtoBuf Ser: " + sw.ElapsedTicks);
-
-            //    sw.Restart();
-            //    var h4 = RequestContentParser.DeserializeHeader(arrayWriter.WrittenSpan);
-            //    //var h2 = System.Text.Json.JsonSerializer.Deserialize<HeaderDto>(arraywriter.WrittenSpan);
-            //    sw.Stop();
-            //    Trace.WriteLine("System.Text Deser: " + sw.ElapsedTicks);
-
-            //    mem2.Position = 0;
-            //    sw.Restart();
-            //    var h3 = ProtoBuf.Serializer.Deserialize<HeaderDto>(mem2);
-            //    sw.Stop();
-            //    Trace.WriteLine("ProtoBuf Deser: " + sw.ElapsedTicks);
-            //}
-
             var listener = new VRpcListener(IPAddress.Any, 1234);
             listener.Start();
             var client = new VRpcClient("localhost", port: 1234, ssl: false, allowAutoConnect: true);
